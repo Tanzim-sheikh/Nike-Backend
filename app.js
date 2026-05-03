72e4fdeff15f106e9e10c40e8f395760ddff45d1
Tanzim Sheikh <tanzimsheikh68@gmail.com>
2026-05-03 14:59:07 +0530

    adding / directory

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+  res.json({ message: 'Backend is running' });
+});
+
 app.use("/api", routes);
 
 // must be last
